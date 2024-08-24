c13cc06be2030fde827a188fda16a6a0ad4c9957
jinalparikh25 <jinalparikh2595@gmail.com>
2024-08-24 14:27:12 +0200

    design of calculator-project

---
+++ projects/4-calculator/calculator/src/Components/Display.jsx
@@ -0,0 +1,12 @@
+import styles from "./Display.module.css"
+
+
+const Display = () => {
+   return (
+        <div>
+            <input className={styles.dispaly} type="text"></input>
+        </div>
+   ) 
+}
+
+export default Display;
